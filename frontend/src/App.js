66a1d440f328bae79e66e0dc08c87c5fc45a29b2
David Gama Serrate <david.serrate@grupocard.com.br>
2024-01-31 21:05:25 -0300

    feat: adicionar chakra-ui no projeto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
 
 export function App() {
   return (
     <>
-      <Header/>
+      <Home/>
     </>
   );
 }

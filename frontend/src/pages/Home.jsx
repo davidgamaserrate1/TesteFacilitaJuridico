8c1714e1c3e3c5aefc8d989d9ad791f8cd120d91
David Gama Serrate <david.serrate@grupocard.com.br>
2024-02-01 08:42:05 -0400

    feat: add form to save new clients

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import { Header } from "../components/Header";
 import './home-styles.css' 
 import { AddClient } from "../components/AddClient";
+import { Content } from "antd/es/layout/layout";
+import { theme } from "antd";
  
 export function Home() {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 return (
     <>
     <Header/>
-    <div className="main_content">
-        
+    <Content className="main_content">
+         
       <div className="top_actions">
         <AddClient />
       </div>
-    </div>
+      </Content>
     </>
   );
 }
+ 

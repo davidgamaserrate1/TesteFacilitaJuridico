c6766fd7198ddf32a12479992286283c71541b69
David Gama Serrate <david.serrate@grupocard.com.br>
2024-02-01 08:09:28 -0400

    refactor: remove chakra-ui library and add ant-design

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client';
 import {App} from './App';
 import './styles-config/global.css'
 
-import { ChakraProvider } from '@chakra-ui/react'
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </ChakraProvider>
 );
  
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,41 +1,17 @@
 import React from "react";
 import { Header } from "../components/Header";
 import './home-styles.css' 
-import { AddIcon} from '@chakra-ui/icons'
-import { Button } from "@chakra-ui/button";
-import { useDisclosure } from "@chakra-ui/hooks";
-import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/modal";
+import { AddClient } from "../components/AddClient";
  
 export function Home() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
 return (
     <>
     <Header/>
     <div className="main_content">
-        <Button onClick={onOpen} leftIcon={<AddIcon />} bg='var(--green-color)' color='var(--white-color)'  variant='outline'>
-            Adicionar cliente
-        </Button>
-        <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-           aaaa
-          </ModalBody>
-
-          <ModalFooter>
-            <Button  colorScheme='blue' mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant='ghost'>Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+        
       <div className="top_actions">
-
-        </div>
+        <AddClient />
+      </div>
     </div>
     </>
   );

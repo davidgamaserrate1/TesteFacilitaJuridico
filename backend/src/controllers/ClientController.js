687c425204c34975b2e056a8e95a259605bca46e
David Gama Serrate <david.serrate@grupocard.com.br>
2024-02-01 13:05:48 -0400

    feat: add update client function with validations

--- backend/src/controllers/ClientController.js
+++ backend/src/controllers/ClientController.js
@@ -79,8 +79,67 @@ export async function listClientById(req, res) {
            
         res.status(200).json(clientFinded)
 	} catch (error) {
-		res.status(500).json({
+		return res.status(500).json({
 			"error": `Erro ao consultar cliente: ${error}`
 		});
 	}
+}
+ 
+
+export async function updateClientById(req, res) {
+    try {
+        const id = req.params.id;
+       
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return res.status(400).json({
+                message: 'Request body is empty. Please provide data for update.',
+            });
+        }
+
+        const querySelect = `SELECT * FROM client WHERE id = $1;`;
+        const clientResult = await dbConnection.query(querySelect, [id]);
+
+        const clientFound = clientResult.rows[0];
+
+        if (!clientFound) {
+            return res.status(404).json({
+                message: `Cliente ${id} não encontrado`,
+            });
+        }
+        
+        const { name, mail, phone, coordinates } = req.body;
+        
+        const clientUpdate = {
+            name: name || clientFound.name,
+            mail: mail || clientFound.mail,
+            phone: phone || clientFound.phone,
+            coordinates: coordinates || clientFound.coordinates,
+        };
+        
+        const isUpdateRequired = (field) => clientUpdate[field] !== clientFound[field];
+
+        if (Object.keys(clientUpdate).some(isUpdateRequired)) {
+            const queryUpdate = `UPDATE client SET name = $1, mail = $2, phone = $3, coordinates = $4 WHERE id = $5;`;
+
+            await dbConnection.query(queryUpdate, [
+                clientUpdate.name,
+                clientUpdate.mail,
+                clientUpdate.phone,
+                clientUpdate.coordinates,
+                id,
+            ]);
+
+            return res.status(200).json({
+                message: `Cliente ${id} atualizado com sucesso!`,
+            });
+        } else {
+            return res.status(200).json({
+                message: 'Por favor, forneça informações diferentes das atuais',
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            error: `Erro ao atualizar cliente: ${error}`,
+        });
+    }
 }
--- backend/src/routes/ClientRoutes.js
+++ backend/src/routes/ClientRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {createClient, listClientById, listClients, removeClientById} from "../Controllers/clientController.js";
+import {createClient, listClientById, listClients, removeClientById, updateClientById} from "../Controllers/clientController.js";
 
 const ClientRouter = express.Router()
 
 ClientRouter
     .get("/", listClients)
     .get("/:id", listClientById)
+    .put("/:id", updateClientById)
     .post("/cadastrar", createClient)
     .delete("/remover/:id", removeClientById)
 
